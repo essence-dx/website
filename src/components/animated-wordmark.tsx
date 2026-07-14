7c2d260065dfa84f3db24e787f852548d9cf07cd
manfromexistence <ajju40959@gmail.com>
2026-07-15 05:04:55 +0600

    feat: add AnimatedWordmark component with randomized directional entry animations

--- src/components/animated-wordmark.tsx
+++ src/components/animated-wordmark.tsx
@@ -6,23 +6,42 @@ import { useEffect, useState } from "react";
 const WORDS = ["I", "Use", "Dx", "BTW"];
 
 const HOLD_MS = 2600;
+const DISTANCE = 100;
+
+const SIDES = [
+  { x: -DISTANCE, y: 0 },
+  { x: DISTANCE, y: 0 },
+  { x: 0, y: -DISTANCE },
+  { x: 0, y: DISTANCE },
+];
+
+const [FALLBACK_SIDE] = SIDES;
+
+function randomSide() {
+  return Math.floor(Math.random() * SIDES.length);
+}
 
 export function AnimatedWordmark() {
   const [index, setIndex] = useState(0);
+  const [side, setSide] = useState(0);
 
   useEffect(() => {
     const id = setInterval(() => {
       setIndex((i) => (i + 1) % WORDS.length);
+      setSide(randomSide());
     }, HOLD_MS);
     return () => clearInterval(id);
   }, []);
+
+  const from = SIDES[side] ?? FALLBACK_SIDE;
+  const to = { x: -from.x, y: -from.y };
 
   return (
     <AnimatePresence mode="wait">
       <motion.span
         key={WORDS[index]}
-        initial={{ x: 100 }}
-        animate={{ x: 0, opacity: 1 }}
+        initial={{ x: from.x, y: from.y, opacity: 1 }}
+        animate={{ x: 0, y: 0, opacity: 1 }}
         transition={{ type: "spring", stiffness: 160, damping: 13 }}
         className="inline-block"
       >
